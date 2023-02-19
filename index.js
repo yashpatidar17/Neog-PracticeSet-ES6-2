322cf0ca0b3013dd500078f887b8de489f3ea591
Yash Patidar <60218098+yashpatidar17@users.noreply.github.com>
2023-02-19 20:10:39 +0000

    Question 1 of practice set 2

---
+++ index.js
@@ -0,0 +1,9 @@
+//Create an object person with two properties, "name" and "age" and then updates the "age" property to a new value. Initial age should be 30.
+
+const obj = {
+  name:"bulla",
+  age:30
+}
+
+obj.age = 35;
+console.log(obj.age);
