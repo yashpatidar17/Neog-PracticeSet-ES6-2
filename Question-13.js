126ee9305f403c3000901c23e2471718d9481a6d
Yash Patidar <60218098+yashpatidar17@users.noreply.github.com>
2023-02-20 20:43:58 +0000

    Question 13

---
+++ Question-13.js
@@ -0,0 +1,4 @@
+//Write a function that takes an array and a number and returns the sum of first element and the number.
+
+const sumFirstElement = (arr,num)=>(arr[0]+num);
+console.log(sumFirstElement([5, 2, 3], 5)); // 6
