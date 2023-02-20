15e09aa983a7cc3906ba931a1b52b72d48605e8f
Yash Patidar <60218098+yashpatidar17@users.noreply.github.com>
2023-02-20 20:39:41 +0000

    Question 11

---
+++ Question-11.js
@@ -0,0 +1,4 @@
+//Write a function that takes an array and returns the last element in the array.
+
+const lastElement = (arr) => arr[arr.length - 1];
+console.log(lastElement([1, 2, 3, 4, 5,6,5,8,9,6,4,1,3,6]));
