5c74acc61ec8afb371541e31699aa7053b219a27
Yash Patidar <60218098+yashpatidar17@users.noreply.github.com>
2023-02-20 16:00:13 +0000

    Question 6

---
+++ Question-5.js
@@ -0,0 +1,11 @@
+//Swap the values of two variables using array destructuring.
+
+let a = 1;
+let b = 2;
+// Your ES6 Code here
+
+[a,b] = [b,a];
+
+
+console.log(a) // 2
+console.log(b) // 1
---
+++ Question-6.js
@@ -0,0 +1,8 @@
+//Convert this function into ES6 with least amount of characters
+
+// function add(a = 30, b = 0) {
+// 	  return a + b;
+// 	}
+
+const add =(a=30,b=0)=> a+b;
+console.log(add(56,2));
