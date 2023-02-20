e7bc1181d37a6fba77b4205bd30c143f0f85714a
Yash Patidar <60218098+yashpatidar17@users.noreply.github.com>
2023-02-20 20:35:39 +0000

    Question 9

---
+++ Question-9.js
@@ -0,0 +1,4 @@
+//Write a function that takes a string as input and returns the string in all uppercase letters
+
+const stringToUpperCase = (str) => str.toUpperCase()
+console.log(stringToUpperCase("hello"));
